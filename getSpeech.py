9c3ade696fe0a8628abdc8a8068c4aa4c94bc5b0
Jarett Mitchell <jarettamitchell@gmail.com>
2023-11-19 16:55:26 -0600

    worked on google search feature.

--- getSpeech.py
+++ getSpeech.py
@@ -2,24 +2,19 @@ import speech_recognition as sr
 from googlesearch import search
 
 
-def googleSearch(Query, numberOfResults):
-    pass
+def googleSearch(Query):  # search using google lib
+    for result in search(Query, num_results=5, stop=5, pause=2):
+        print(result)
 
 
+# initialize recognition
 recognizer = sr.Recognizer()
 
 with sr.Microphone() as source:
     print("Speak")
     audio = recognizer.listen(source)
 
-# if "search google" in audio:
-#     print('What would you like to search for?')
-#     query = recognizer.listen(source)
-#     print('How many results would you like?')
-#     resultNumbers = recognizer.listen(source)
-#     googleSearch()
-
-try:
+try:  # test if speech was recognized
     print("You said: " + recognizer.recognize_google(audio))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand the audio")
@@ -27,11 +22,12 @@ except sr.RequestError as e:
     print(
         "Could not request results from Google Speech Recognition service; {0}".format(e))
 
-try:
+try:  # search google if commanded FIXME
     recognized_text = recognizer.recognize_google(audio).lower()
-    if "search google" in recognized_text:
+    if "search google" or "Google search" in recognized_text:
         with sr.Microphone() as source:
             print("what for?")
             searchRecognizer = recognizer.listen(source)
+            googleSearch(recognizer.recognize_google(searchRecognizer))
 except:
     print("search failed...")
